6446792a77840e18ba1d559337f35cabc3fbbd26
Allan Camargo <allan.mdev@gmail.com>
2025-02-22 19:14:58 -0300

    feat: aula 3

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,30 @@
+import { notFound } from "next/navigation";
+
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+
+  const restaurant = await getRestaurantBySlug(slug);
+
+  return <h1>Menu: {slug}</h1>;
+};
+
+export default RestaurantMenuPage;
